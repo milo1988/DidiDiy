a081235ee67a0cf4653f2c6c21f8f93e0eedae4b
milo <milotian1988@gmail.com>
2016-05-12 23:42:52 +0800

    have a try

--- src/com/milo/pokermark/Test.java
+++ src/com/milo/pokermark/Test.java
@@ -3,6 +3,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
 		String str = "sdl;j";
 		System.out.print(str + "\r\n");
 		str = "sddddddddddddddfd";
